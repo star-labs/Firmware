da9c136fd56f761858d292e94d76877b14608fb5
Espen Nilsen <espen.k.nilsen@gmail.com>
2013-05-14 21:46:50 +0200

    Lagt til flere filer som bb_handler benytter, samt gjort endringer i mavlink og uOrb topics

---
+++ mavlink/include/mavlink/v1.0/image_metadata/version.h
@@ -0,0 +1,12 @@
+/** @file
+ *	@brief MAVLink comm protocol built from image_metadata.xml
+ *	@see http://pixhawk.ethz.ch/software/mavlink
+ */
+#ifndef MAVLINK_VERSION_H
+#define MAVLINK_VERSION_H
+
+#define MAVLINK_BUILD_DATE "Wed May 01 21:00:16 2013"
+#define MAVLINK_WIRE_PROTOCOL_VERSION "1.0"
+#define MAVLINK_MAX_DIALECT_PAYLOAD_SIZE 91
+ 
+#endif // MAVLINK_VERSION_H
